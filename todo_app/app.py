45309594d1b5d7a5021f2424fa55d42970eb226e
Simeon Penev <simeon_penev@hotmail.com>
2021-08-19 18:29:27 +0300

    View list of items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 from todo_app.flask_config import Config
+import todo_app.data.session_items as session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,8 +10,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
-
+    return render_template('index.html', items = session.get_items())
 
 if __name__ == '__main__':
     app.run()
