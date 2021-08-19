e112b2a5b4eafc1499f5ec4ca94e3fd2f5e689b9
Simeon Penev <simeon_penev@hotmail.com>
2021-08-19 18:39:16 +0300

    Revert "View list of items"
    This reverts commit 45309594d1b5d7a5021f2424fa55d42970eb226e.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from flask import render_template
 
 from todo_app.flask_config import Config
-import todo_app.data.session_items as session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,7 +8,8 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html', items = session.get_items())
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
