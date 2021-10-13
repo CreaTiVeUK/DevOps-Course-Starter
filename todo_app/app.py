f0bdbef6d719016ad96210c5e2fb4b88a29d6080
Simeon Penev <simeon_penev@hotmail.com>
2021-10-13 21:34:44 +0100

    Added functionality to add new titles

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask.globals import request
+from flask.wrappers import Request
 from todo_app.flask_config import Config
+import todo_app.data.session_items as session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +10,14 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items = session.get_items())
+
+
+@app.route('/add_item', methods=['POST'])  
+def add_item():
+    title = request.form['filmTitle']
+    session.add_item(title)
+    return index()
 
 
 if __name__ == '__main__':
