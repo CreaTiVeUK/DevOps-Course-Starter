6b783275afbf8b9b458474ef0f866c97b55d1597
Simeon Penev <simeon_penev@hotmail.com>
2022-01-10 21:15:14 +0200

    Removed 1 import, add_film() now redirects, Changed function name. Suggested changes complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask.globals import request
-from flask.wrappers import Request
 from todo_app.flask_config import Config
 import todo_app.data.session_items as session
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,11 +13,11 @@ def index():
     return render_template('index.html', items = session.get_items())
 
 
-@app.route('/add_item', methods=['POST'])  
-def add_item():
+@app.route('/add_film', methods=['POST'])  
+def add_film():
     title = request.form['filmTitle']
-    session.add_item(title)
-    return index()
+    session.add_film(title)
+    return redirect("/", code=302)
 
 
 if __name__ == '__main__':
