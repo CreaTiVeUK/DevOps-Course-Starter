a9e645b07ce2abb55645b77cb5f04f5bfcb179ca
Simeon Penev <simeon_penev@hotmail.com>
2022-09-07 02:58:29 +0300

    Added unauthorized handler and mostly done callback route

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect
+import requests
 from todo_app.data.configs.flask_config import Config
 from todo_app.data.view_model import ViewModel
 from todo_app.data.mongo_db_client import MongoDBClient
+from flask_login import LoginManager, login_required
 import todo_app.data.configs.logger_settings as logger
 
 def create_app():
@@ -9,9 +11,44 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     mongo_db = MongoDBClient()
+
+    login_manager = LoginManager()
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        return redirect(f"https://github.com/login/oauth/authorize?client_id={app.config['GITHUB_CLIENT_ID']}")
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        pass # We will return to this later
+
+    login_manager.init_app(app)
     
+    @app.route('/login/callback')
+    def callback():
+        code = request.args.get('code')
+        token_headers = {
+            "Accept": 'application/json'
+        }
+        token_body = {
+            "client_id": app.config['GITHUB_CLIENT_ID'],
+            "client_secret": app.config['GITHUB_CLIENT_SECRET'],
+            "code": code
+        }
+        url = f"https://github.com/login/oauth/access_token"
+        response=requests.post(url, token_body, headers = token_headers)
+        access_token = response.json()['access_token']
+        auth_headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer " + access_token
+        }
+        auth_url = 'https://api.github.com/user'
+        auth_response = requests.get(auth_url, headers = auth_headers)
+
+        return auth_response.json()
 
     @app.route('/')
+    @login_required
     def index():
         items = mongo_db.get_items()
         item_view_model = ViewModel(items) 
@@ -19,6 +56,7 @@ def create_app():
 
 
     @app.route('/add_item', methods=['POST'])
+    @login_required
     def add_item():
         title = request.form['filmTitle']
         logger.log.info('Title to add: ' + title)
@@ -27,6 +65,7 @@ def create_app():
 
 
     @app.route('/item_description/<string:titleID>', methods=['GET', 'POST'])
+    @login_required
     def item_description(titleID):
         description = request.form['description']
         logger.log.info('Card ID\'s description to update: ' + titleID)
@@ -36,6 +75,7 @@ def create_app():
 
 
     @app.route('/item_due/<string:titleID>', methods=['GET', 'POST'])
+    @login_required
     def item_due(titleID):
         due = request.form['date']
         logger.log.info('Card ID\'s : ' + titleID)
@@ -45,6 +85,7 @@ def create_app():
 
 
     @app.route('/complete_item/<string:titleID>', methods=['POST'])
+    @login_required
     def complete_item(titleID):
         logger.log.info('Attempting to move card to Done list: '+ titleID)
         mongo_db.update_document(titleID, 'status', 'Done')
@@ -52,6 +93,7 @@ def create_app():
 
 
     @app.route('/doing_item/<string:titleID>', methods=['POST'])
+    @login_required
     def doing_item(titleID):
         logger.log.info('Attempting to move card to Doing list: '+ titleID)
         mongo_db.update_document(titleID, 'status', 'Doing')
@@ -59,6 +101,7 @@ def create_app():
 
 
     @app.route('/revert_item/<string:titleID>', methods=['POST'])
+    @login_required
     def revert_item(titleID):
         logger.log.info('Attempting to move card to To Do list: '+ titleID)
         mongo_db.update_document(titleID, 'status', 'To Do')
--- todo_app/data/configs/flask_config.py
+++ todo_app/data/configs/flask_config.py
@@ -5,10 +5,14 @@ class Config:
         """Base configuration variables."""
         self.MONGO_CON_STRING= os.environ.get('MONGO_CON_STRING')
         self.MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME')
+        self.GITHUB_CLIENT_ID=os.environ.get('GITHUB_CLIENT_ID')
+        self.GITHUB_CLIENT_SECRET=os.environ.get('GITHUB_CLIENT_SECRET')
         
         ENV_VARIABLES = {
             self.MONGO_CON_STRING,
-            self.MONGO_DB_NAME
+            self.MONGO_DB_NAME,
+            self.GITHUB_CLIENT_ID,
+            self.GITHUB_CLIENT_SECRET
             }
         
         for i in ENV_VARIABLES:
